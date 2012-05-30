a8f9beed67e676bc48bad5a692142b09c95dc8fb
Aaron Hicks <hicksa@landcareresearch.co.nz>
2012-05-30 15:59:34 +1200

    .provide requires an argument?

--- lib/puppet/provider/mongoadmin/mongoadmin.rb
+++ lib/puppet/provider/mongoadmin/mongoadmin.rb
@@ -1,4 +1,4 @@
-Puppet::Type.type(:mongoadmin).provide do
+Puppet::Type.type(:mongoadmin).provide(:ruby) do
 
   def exists?
     output = `echo 'show users'|mongo -u #{self[:admin]} -p #{self[:password]} #{self[:host]}:#{self[:port]}/admin}`
@@ -6,5 +6,7 @@ Puppet::Type.type(:mongoadmin).provide do
     return false unless result
     output =~ /"user" : "#{self[:admin]}",/
   end
+
+
 
 end
